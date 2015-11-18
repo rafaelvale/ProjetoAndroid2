d04e1aa6e63a833a5b43c81309347d4feb4ee2b8
rafaelvale <faelvale@gmail.com>
2015-11-18 08:44:16 -0200

    Tela Login com validacao

--- app/src/main/java/com/stg/achados/achadoseperdidos/ActivityLogin.java
+++ app/src/main/java/com/stg/achados/achadoseperdidos/ActivityLogin.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -11,11 +14,59 @@ import android.view.View;
  */
 public class ActivityLogin extends Activity{
 
+
+    EditText edtNome;
+    EditText edtSenha;
+    Button btnCadastrar;
+    //variavel para validar os dados do formulario
+    Boolean camposValidados;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+        edtNome = (EditText)findViewById(R.id.edtNome);
+        edtSenha = (EditText)findViewById(R.id.edtSenha);
+        btnCadastrar = (Button)findViewById(R.id.btnCadastrar);
+
+        //Programando o botao cadastrar
+        btnCadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                camposValidados = true;
+
+                if(edtNome.getText().length()==0 || edtSenha.getText().length()==0){
+                    Toast.makeText(getApplication(),"Favor informar usuário e senha",Toast.LENGTH_LONG).show();
+
+                    camposValidados = false;
+                }else{
+
+                    Toast.makeText(getApplication(),"Seja Bem Vindo "+edtNome.getText().toString()+"!",Toast.LENGTH_LONG).show();
+
+                    edtNome.setText("");
+                    edtSenha.setText("");
+
+                    Intent intent = new Intent(ActivityLogin.this, ActivityHome.class);
+                    startActivity(intent);
+
+                }
+                if(camposValidados){
+                    Toast.makeText(getApplication(),"Usuário Autenticado com Sucesso!",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(ActivityLogin.this, ActivityHome.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(getApplication(),"Dados Inválidos!",Toast.LENGTH_LONG).show();
+
+
+                }
+            }
+        });
     }
+
+
 
     public void CriarCadastro(View view){
 
@@ -23,17 +74,5 @@ public class ActivityLogin extends Activity{
         startActivity(intent);
     }
 
-    public void Acessar(View view){
 
-        Intent intent = new Intent(this, ActivityHome.class);
-        startActivity(intent);
-    }
-
-    public void APIGplus(View view){
-
-    }
-
-    public void APIFace(View view){
-
-    }
 }
